b7baf8f1561f8701f52883345ed2588e8da50553
Martin <martin_krumov@abv.bg>
2017-10-23 11:09:48 +0300

    Changed fields in TransactionDAO

--- src/main/java/com/dao/classes/TransactionDAO.java
+++ src/main/java/com/dao/classes/TransactionDAO.java
@@ -6,6 +6,7 @@ import com.model.Transaction;
 import com.model.TransactionType;
 
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.Set;
 
 public class TransactionDAO {
@@ -13,19 +14,19 @@ public class TransactionDAO {
 
     public synchronized void insertTransaction(Transaction t) throws SQLException {
 
-        String query = "INSERT INTO finance_tracker.transactions (type, amount, description, date, account_id, category_id) VALUES (?, ?, ?, ?, ?)";
+        String query = "INSERT INTO finace_tracker.transactions (type, amount, description, date, category_id) VALUES (?, ?, ?, ?, )";
         PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
         statement.setString(1, t.getType().toString());
-        statement.setTimestamp(2, Timestamp.valueOf(t.getDate().withNano(0)));
+        statement.setTimestamp(2, Timestamp.valueOf(LocalDateTime.now()));
         statement.setBigDecimal(3, t.getAmount());
         statement.setString(4, t.getDescription());
-        statement.setLong(5, t.getAccount());
-        statement.setLong(6, t.getCategory());
+        statement.setLong(5, t.getCategoryId());
         statement.executeUpdate();
 
         ResultSet resultSet = statement.getGeneratedKeys();
         resultSet.next();
         t.setTransactionId(resultSet.getLong(1));
+
         try {
             connection.setAutoCommit(false);
 
