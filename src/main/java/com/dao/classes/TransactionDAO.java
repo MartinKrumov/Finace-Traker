a341855ff33a4700cda792718d08036563309733
Martin <martin_krumov@abv.bg>
2017-10-27 03:02:49 +0300

    Fixed bug in insertTransaction()

--- src/main/java/com/dao/classes/TransactionDAO.java
+++ src/main/java/com/dao/classes/TransactionDAO.java
@@ -11,20 +11,22 @@ import java.util.Set;
 
 public class TransactionDAO {
     private static Connection connection;
+    private static TransactionDAO instance;
+
+    public static final String INSERT_TRANSACTION ="INSERT INTO transactions (transaction_type, amount, date, description, category_id) VALUES (?, ?, ?, ?, ?)";
 
     public synchronized void insertTransaction(Transaction t) throws SQLException {
         connection = DBConnection.getInstance().getConnection();
-        String query = "INSERT INTO finace_tracker.transactions (type, amount, description, date, category_id) VALUES (?, ?, ?, ?, )";
 
-        PreparedStatement prepareStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-        prepareStatement.setString(1, t.getType().toString());
-        prepareStatement.setTimestamp(2, Timestamp.valueOf(LocalDateTime.now()));
-        prepareStatement.setBigDecimal(3, t.getAmount());
-        prepareStatement.setString(4, t.getDescription());
-        prepareStatement.setLong(5, t.getCategoryId());
-        prepareStatement.executeUpdate();
+        PreparedStatement ps = connection.prepareStatement(INSERT_TRANSACTION, Statement.RETURN_GENERATED_KEYS);
+        ps.setString(1, t.getType().toString());
+        ps.setBigDecimal(2, t.getAmount());
+        ps.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
+        ps.setString(4, t.getDescription());
+        ps.setLong(5, t.getCategoryId());
+        ps.executeUpdate();
 
-        ResultSet resultSet = prepareStatement.getGeneratedKeys();
+        ResultSet resultSet = ps.getGeneratedKeys();
         resultSet.next();
         t.setTransactionId(resultSet.getLong(1));
 
@@ -39,4 +41,13 @@ public class TransactionDAO {
             connection.setAutoCommit(true);
         }
     }
+
+    public synchronized static TransactionDAO getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new TransactionDAO();
+        }
+        return instance;
+    }
+
+
 }
--- src/main/java/com/model/Demo.java
+++ src/main/java/com/model/Demo.java
@@ -1,17 +1,30 @@
 package com.model;
 
 import com.dao.classes.CategoryDAO;
+import com.dao.classes.TransactionDAO;
 
+import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 
 public class Demo {
     public static void main(String[] args) {
-        Category c = new Category("TONKA SERVLETA99", TransactionType.EXPENCE, "asdasd", "yes", 1, 1);
+//        Category c = new Category("TONKA SERVLETA99", TransactionType.EXPENCE, "asdasd", "yes", 1, 1);
+//
+//        try {
+//            CategoryDAO.insertCategory(c);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+
+        Transaction t = new Transaction(TransactionType.EXPENCE, BigDecimal.ONE.valueOf(20.5), LocalDateTime.now(), "Just runn", 14);
 
         try {
-            CategoryDAO.insertCategory(c);
+            TransactionDAO dao = TransactionDAO.getInstance();
+            dao.insertTransaction(t);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
     }
 }
