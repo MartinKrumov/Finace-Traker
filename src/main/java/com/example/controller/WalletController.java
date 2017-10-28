12b725cf97b7f9ce062cbacafbe61aef67972661
Martin <martin_krumov@abv.bg>
2017-10-28 23:39:44 +0300

    Added wallet.jsp, WalletController , getAllWallets()

--- src/main/java/com/example/controller/WalletController.java
+++ src/main/java/com/example/controller/WalletController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import com.example.model.dao.WalletDAO;
 import com.example.model.pojo.User;
 import com.example.model.pojo.Wallet;
 import org.springframework.stereotype.Controller;
@@ -16,6 +17,9 @@ public class WalletController {
     @RequestMapping(value = "/wallets", method = RequestMethod.GET)
     public String getAllWallets(HttpSession session, Model model) {
         User user = (User) session.getAttribute("user");
-        Set<Wallet> wallets =
+        Set<Wallet> wallets = WalletDAO.selectUserWallets(user.getUserId());
+        model.addAttribute("wallets", wallets);
+
+        return "wallets";
     }
 }
--- src/main/webapp/WEB-INF/views/jsp/wallets.jsp
+++ src/main/webapp/WEB-INF/views/jsp/wallets.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: Martin
@@ -11,6 +12,19 @@
     <title>Wallets</title>
 </head>
 <body>
-    <h1>User: ${user.name}</h1>
+    <h1>User: ${sessionScope.user.username}</h1>
+
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Amount</th>
+        </tr>
+        <c:forEach items="${wallets}" var="w">
+            <tr>
+                <td>${w.name}</td>
+                <td>${w.amount}</td>
+            </tr>
+        </c:forEach>
+    </table>
 </body>
 </html>
