95f0c4ddfdc5b2a78b93aa05825748803c41aab2
ToniPopov <tonitheonea@gmail.com>
2017-10-29 16:52:46 +0200

    session fixed for login user

--- src/main/java/com/example/controller/LoginSignUpController.java
+++ src/main/java/com/example/controller/LoginSignUpController.java
@@ -71,7 +71,7 @@ public class LoginSignUpController {
                     response.addCookie(cookie);
                 }
         }
-        return "home";
+        return "redirect:index";
     }
 
 }
